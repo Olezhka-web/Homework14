ab8b2917ccd75d421d8a11c22694aeeacaaa7710
Olezhka-web <zcegthrkfcghj5@gmail.com>
2020-12-03 12:29:34 +0200

    master

--- src/App.js
+++ src/App.js
@@ -1,29 +1,11 @@
 import React, {Component} from 'react';
-import UserComponents from "./components/user/UserComponents";
-
 
 class App extends Component {
-        users = [
-				{name: 'vasya', age: 31, status: false},
-				{name: 'petya', age: 30, status: true},
-				{name: 'kolya', age: 29, status: true},
-				{name: 'olya', age: 28, status: false},
-				{name: 'max', age: 30, status: true},
-				{name: 'anya', age: 31, status: false},
-				{name: 'oleg', age: 28, status: false},
-				{name: 'andrey', age: 29, status: true},
-				{name: 'masha', age: 30, status: true},
-				{name: 'olya', age: 31, status: false},
-				{name: 'max', age: 31, status: true}
-			];
+
     render() {
         return (
             <div>
-				{
-					this.users.map((user, index) => {
-						return (<UserComponents user={user} key={index}/>)
-					} )
-				}
+
             </div>
         );
     }
