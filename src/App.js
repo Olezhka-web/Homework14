66024d41abf718e448fbdea4bd8be1a50e61ff53
Olezhka-web <zcegthrkfcghj5@gmail.com>
2020-12-18 22:55:36 +0200

    ReactHomework-6

--- src/App.js
+++ src/App.js
@@ -1,44 +1,16 @@
 import React, {Component} from 'react';
+import Form from "./components/Form";
 
 
 class App extends Component {
-    myForm = React.createRef();
-    state = {inputValue: '', users: [], findUserApi: []}
-
-    componentDidMount() {
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then(value => value.json())
-            .then(usersFromAPI => {
-                this.setState({users: usersFromAPI});
-            });
-    }
-
     render() {
-        let {users, findUserApi} = this.state;
         return (
             <div>
-                <form action={'/save'} onSubmit={this.send} ref={this.myForm}>
-                    <input type={'number'} onInput={this.commitState} value={this.state.inputValue}/>
-                    <button>send</button>
-                </form>
                 <div>
-                    {findUserApi.id}-{findUserApi.name}-{findUserApi.username}-{findUserApi.email}
+                    <Form/>
                 </div>
             </div>
         );
-    }
-    send = (e) =>{
-        e.preventDefault();
-        console.log(this.state.inputValue);
-        this.findUser();
-    }
-
-    commitState = (e) => {
-        this.setState({inputValue: e.target.value})
-    }
-    findUser = () =>{
-        let findUserApi = this.state.users.find(value => value.id.toString() === this.state.inputValue);
-        this.setState({findUserApi});
     }
 }
 
