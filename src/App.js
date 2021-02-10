c359258d1864bf942ca036f8948dc69015917e1c
Olezhka-web <zcegthrkfcghj5@gmail.com>
2021-02-10 18:10:29 +0200

    Update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import React, {Component} from 'react';
+import React, {useEffect} from 'react';
+import {useDispatch, useSelector} from "react-redux";
 
 
-class App extends Component {
-    render() {
-        return (
-            <div>
-
-            </div>
-        );
-    }
-}
-
-export default App;
+export default function App(){
+    const users = useSelector(({id, name, username}) => ({id, name, username}));
+    useEffect(()=> {
+        fetch(`https://jsonplaceholder.typicode.com/users/${users.id}`)
+            .then(value => value.json())
+            .then(value => dispatch({type: 'SET_ACTION', payload: value}));
+    }, [users.id]);
+    const dispatch = useDispatch();
+    return(
+        <div>
+            <button onClick={()=> dispatch({type: 'ID_INCREMENT'})}>Next User</button>
+            <button onClick={()=> dispatch({type: 'ID_DECREMENT'})}>Previous User</button>
+            <button onClick={() => dispatch({type: 'CHANGE_NAME', payload: 'Hello'})}>Change Name</button>
+            <button onClick={() => dispatch({type: 'CHANGE_USERNAME', payload: 'World'})}>Change Username</button>
+            <h1>Id: {users.id}</h1>
+            <h1>Name: {users.name}</h1>
+            <h1>Username: {users.username}</h1>
+        </div>
+    );
+};
--- src/index.js
+++ src/index.js
@@ -3,10 +3,44 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createStore} from "redux";
+import {Provider} from "react-redux";
+const initialState ={
+    id: 1,
+    name: '',
+    username: ''
+}
+
+const reducer = (state = initialState, action) =>{
+    switch (action.type){
+        case 'SET_ACTION': {
+            return action.payload
+        }
+        case 'ID_INCREMENT': {
+            return {...state, id: state.id + 1}
+        }
+        case 'ID_DECREMENT': {
+            return {...state, id: state.id - 1}
+        }
+        case 'CHANGE_NAME': {
+            return {...state, name: action.payload}
+        }
+        case 'CHANGE_USERNAME':{
+            return {...state, username: action.payload}
+        }
+        default:{
+            return state;
+        }
+    }
+}
+
+const store = createStore(reducer);
 
 ReactDOM.render(
   <React.StrictMode>
+      <Provider store={store}>
     <App />
+      </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
